cf1b55c2e5bf693295b31ee08989805c0b1c5f90
sohseyedi <sohseyedi@gmail.com>
2025-09-06 18:42:19 +0330

    feat:create client and enable enable logging in development only

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,35 +1,5 @@
-import { Client, Pool } from 'pg';
-import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
 
-dotenv.config();
-
-const POSTGRES_URL = process.env.POSTGRES_URL!;
-const MAIN_DB = process.env.MAIN_DB!;
-
-export const createDatabase = async (dbName: string) => {
-  const client = new Client({ connectionString: POSTGRES_URL });
-  try {
-    await client.connect();
-    const checkDb = await client.query(`SELECT 1 FROM pg_database WHERE datname=$1`, [dbName]);
-    if (checkDb.rowCount === 0) {
-      await client.query(`CREATE DATABASE "${dbName}"`);
-      console.log(`Database Created`);
-    } else {
-      console.log(`🔥 Database Connected`);
-    }
-  } catch (err) {
-    console.error('❌ Error creating database:', err);
-  } finally {
-    await client.end();
-  }
-};
-
-export const pool = new Pool({
-  connectionString: MAIN_DB,
-});
-
-process.on('SIGINT', async () => {
-  await pool.end();
-  console.log('Database pool closed');
-  process.exit(0);
+export const prisma = new PrismaClient({
+  log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : [],
 });
