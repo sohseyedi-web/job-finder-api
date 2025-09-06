124697e5fc48816ad88ad30f277c1dfa524220f4
sohseyedi <sohseyedi@gmail.com>
2025-09-06 18:39:36 +0330

    feat:create database and schema with prisma

---
+++ prisma/migrations/20250906071920_init/migration.sql
@@ -0,0 +1,44 @@
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('USER', 'OWNER', 'ADMIN', 'EMPLOYEE');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "public"."Role" NOT NULL DEFAULT 'USER',
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Profile" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "city" TEXT,
+    "phoneNumber" TEXT,
+    "jobTitle" TEXT,
+    "resumeUrl" TEXT,
+    "companyName" TEXT,
+    "companyCity" TEXT,
+    "address" TEXT,
+    "companyPhone" TEXT,
+    "website" TEXT,
+    "logoUrl" TEXT,
+    "ownerPhone" TEXT,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "public"."Profile"("userId");
+
+-- AddForeignKey
+ALTER TABLE "public"."Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
