e7f9c01bd9f0162b926f5434178f545a2960ab50
sohseyedi <sohseyedi@gmail.com>
2025-09-06 18:43:56 +0330

    chore:check file when complete role owner or user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,13 +1,36 @@
 import { completeProfile, getUserProfile, updateProfile } from '@/controllers/user.controller';
 import { verifyAccessToken } from '@/middleware/user.middleware';
-import { uploadPDF } from '@/utils/multer';
+import { uploadImage, uploadPDF } from '@/utils/multer';
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 
 const router = express.Router();
 router.use(verifyAccessToken);
 router.get('/profile', expressAsyncHandler(getUserProfile));
-router.post('/complete', uploadPDF.single('resume'), expressAsyncHandler(completeProfile));
-router.patch('/update', uploadPDF.single('resume'), expressAsyncHandler(updateProfile));
+router.post(
+  '/complete',
+  (req, res, next) => {
+    if ((req as any).user?.role === 'USER') {
+      return uploadPDF.single('resume')(req, res, next);
+    } else if ((req as any).user?.role === 'OWNER') {
+      return uploadImage.single('logo')(req, res, next);
+    }
+    next();
+  },
+  expressAsyncHandler(completeProfile)
+);
+
+router.patch(
+  '/update',
+  (req, res, next) => {
+    if ((req as any).user?.role === 'USER') {
+      return uploadPDF.single('resume')(req, res, next);
+    } else if ((req as any).user?.role === 'OWNER') {
+      return uploadImage.single('logo')(req, res, next);
+    }
+    next();
+  },
+  expressAsyncHandler(updateProfile)
+);
 
 export default router;
