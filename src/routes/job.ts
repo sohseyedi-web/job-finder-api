6f6b1d92edd1d69a15beb1c69255aac6dbf22e59
sohseyedi <sohseyedi@gmail.com>
2025-09-07 13:57:04 +0330

    feat: create job routes hanlder

---
+++ src/routes/job.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import {
+  createJob,
+  applyForJob,
+  getJobApplications,
+  updateApplicationStatus,
+  getJobs,
+} from '@/controllers/job.controller';
+import { verifyAccessToken } from '@/middleware/user.middleware';
+
+const router = Router();
+
+router.get('/lists', expressAsyncHandler(getJobs));
+router.use(verifyAccessToken);
+router.get('/:jobId', expressAsyncHandler(getJobApplications as any));
+router.post('/create', expressAsyncHandler(createJob as any));
+router.post('/apply', expressAsyncHandler(applyForJob as any));
+router.patch('/:applicationId', expressAsyncHandler(updateApplicationStatus as any));
+
+export default router;
