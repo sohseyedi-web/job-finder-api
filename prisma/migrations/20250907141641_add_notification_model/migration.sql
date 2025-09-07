0b47572f82f970674f34655f6dd3b215fb34aaaf
sohseyedi <sohseyedi@gmail.com>
2025-09-07 17:58:35 +0330

    chore : add notification database model

---
+++ prisma/migrations/20250907141641_add_notification_model/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "public"."NotificationType" AS ENUM ('SYSTEM', 'JOB', 'TICKET');
+
+-- CreateTable
+CREATE TABLE "public"."Notification" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "type" "public"."NotificationType" NOT NULL,
+    "recipientId" TEXT NOT NULL,
+    "senderId" TEXT NOT NULL,
+    "senderName" TEXT NOT NULL,
+    "isRead" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."Notification" ADD CONSTRAINT "Notification_recipientId_fkey" FOREIGN KEY ("recipientId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
