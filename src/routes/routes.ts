c841b058e3929a0a968fae5456994d25c9c92bff
sohseyedi <sohseyedi@gmail.com>
2025-09-07 13:57:16 +0330

    chore:add job routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import authRoutes from './auth';
 import userRoutes from './user';
+import jobRoutes from './job';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
+router.use('/job', jobRoutes);
 
 export default router;
