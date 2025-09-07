07968dcf9a6f22cd0529c718b0c22d2cd0e30c3d
sohseyedi <sohseyedi@gmail.com>
2025-09-07 08:39:09 +0330

    chore:add new user types

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,14 +1,33 @@
+export type Role = 'USER' | 'OWNER' | 'ADMIN' | 'EMPLOYEE';
+
+export interface IProfile {
+  id: string;
+  userId: string;
+  city?: string | null;
+  phoneNumber?: string | null;
+
+  // USER
+  jobTitle?: string | null;
+  resumeUrl?: string | null;
+
+  // OWNER
+  companyName?: string | null;
+  companyCity?: string | null;
+  address?: string | null;
+  companyPhone?: string | null;
+  website?: string | null;
+  logoUrl?: string | null;
+  ownerPhone?: string | null;
+}
+
 export interface IUser {
   id: string;
   fullName: string;
   email: string;
   password: string;
-  is_active: boolean;
-  jobTitle?: string;
-  resume?: string;
-  phoneNumber?: string;
-  city?: string;
-  role: 'user' | 'admin' | 'owner';
-  created_at: Date;
-  updated_at: Date;
+  isActive: boolean;
+  role: Role;
+  createdAt: Date;
+  updatedAt: Date;
+  profile?: IProfile | null;
 }
