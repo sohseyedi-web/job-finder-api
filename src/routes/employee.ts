ddab1aa56956a83239aebcac8f68f28dafa9b249
sohseyedi <sohseyedi@gmail.com>
2025-09-08 17:45:08 +0330

    chore : add new employee route handler

--- src/routes/employee.ts
+++ src/routes/employee.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
-import { changeActiveOwnerJob } from '../controllers/employee.controller';
+import {
+  changeActiveOwnerJob,
+  getAllNotifications,
+  getProcessesLists,
+  markNotificationAsReadForEmployee,
+} from '../controllers/employee.controller';
 import { verifyAccessToken } from '@/middleware/user.middleware';
 
 const router = express.Router();
 
 router.use(verifyAccessToken);
 router.patch('/change-active', expressAsyncHandler(changeActiveOwnerJob as any));
-router.get('/process', expressAsyncHandler(changeActiveOwnerJob as any));
+router.get('/process', expressAsyncHandler(getProcessesLists as any));
+router.get('/all-notifications', expressAsyncHandler(getAllNotifications as any));
+router.patch('/read/:id', expressAsyncHandler(markNotificationAsReadForEmployee as any));
 
 export default router;
