dc1cc3996b9ffc11dd46074dc775a8e04573c724
sohseyedi <sohseyedi@gmail.com>
2025-09-08 17:33:24 +0330

    chore: add employee routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,7 @@ import authRoutes from './auth';
 import userRoutes from './user';
 import jobRoutes from './job';
 import notificationRoutes from './notification';
+import employeeRoutes from './employee';
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
 router.use('/job', jobRoutes);
 router.use('/notification', notificationRoutes);
+router.use('/employee', employeeRoutes);
 
 export default router;
